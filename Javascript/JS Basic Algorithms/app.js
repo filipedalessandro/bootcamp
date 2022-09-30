26b8d84a196d4d16d7fd40c29db19b563cf6b1cf
filipedalessandro <filipedalessandro@gmail.com>
2022-09-30 19:33:53 +0200

    30.09

--- Javascript/JS Basic Algorithms/app.js
+++ Javascript/JS Basic Algorithms/app.js
@@ -23,25 +23,34 @@ var z = h + y
 //1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 //const character = {name: 'Jack Sparrow', age: 10};
 
+const character = {name: 'Jack Sparrow', age: 10};
+character ["age"] = 25
 
 
-//1.2 Declara 3 variables con los nombres y valores siguientes 
-//firstName = 'Jon'; 
-//lastName = 'Snow'; 
-//age = 24; 
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+firstName = 'Jon'; 
+lastName = 'Snow'; 
+age = 24;
 Muestralos por consola de esta forma: 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
 
 let firstName = 'Jon'
 let lastName = 'Snow'
 let age = 24
 
-const 
+let phrase = "Soy" + firstName + " " + lastName + ", " + "tengo" + age + " años y me gustan los lobos"
+
+console.log(phrase)
 
 /*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes.
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};*/
 
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
 
+const totalJuguetes = toy1.price() + toy2.price()
+
+console.log(totalJuguetes)
 
 /*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad basePrice más 
